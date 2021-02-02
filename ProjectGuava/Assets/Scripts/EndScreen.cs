eb6d6383ee563a3a220140a71da90e7d811b1d21
foxxyiness <foxxyiness@users.noreply.github.com>
2021-02-02 13:12:08 -0500

    Add Script and Finishes Scene UI
    UI of the screen is finished and adds the script for the buttons

---
+++ ProjectGuava/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+   public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
