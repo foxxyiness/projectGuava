c4229b185e90467cd5287210b8d3aba9ba197d98
foxxyiness <coachfoxjp@gmail.com>
2021-02-02 23:14:22 -0500

    Test Build #2
    made another test build to make sure everything is running smoothly. Health Script mostly finished only need to add respawn

--- ProjectGuava/Assets/Scripts/Health.cs
+++ ProjectGuava/Assets/Scripts/Health.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isDead;
+    public int maxHealth = 3;
+    public int currentHealth;
     void Start()
     {
-        
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentHealth = maxHealth;
+        if (gameObject.transform.position.y < -6)
+        { 
+            isDead = true;
+        }
+
+        if (isDead == true)
+        {
+            currentHealth = currentHealth - 1;
+            StartCoroutine("Die");
+            SceneManager.LoadScene(1);
+        }
+
+        if (currentHealth == 0)
+        {
+            //Loads death screen :D
+            SceneManager.LoadScene(2);
+        }
+    }
+    IEnumerator Die()
+    {
+        Debug.Log("Player has fallen");
+        yield return new WaitForSeconds(2);
+        Debug.Log("Player dead");
     }
 }
